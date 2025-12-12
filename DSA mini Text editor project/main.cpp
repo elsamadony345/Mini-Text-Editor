d92e3c5a7bca216144d153a16e33c3ba8629cd4c
mohamed elsamadony <elsamadony345@gmail.com>
2025-12-12 16:46:30 +0200

    Initial Commit

---
+++ DSA mini Text editor project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DSA mini Text editor project
+//
+//  Created by Mohamed Elsamadony on 2025-12-12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
